378af0c6b7029e2bb18fa88e55477dfeabf5b9fc
Arjun Kalidas <arjunkalidas@Arjuns-MacBook-Pro.local>
2023-11-10 00:09:01 -0500

    updating the todo items, renaming the files to match the mission, updating package json and import statements in other classes

---
+++ FF-1/FPEncryption.js
@@ -0,0 +1,6 @@
+'use strict';
+
+
+// TODO:
+// This is the equivalent of wrapper, please remove this comment and start fresh
+// Move the radixedIntArray conversion functions to commonUtil and consume from there.
---
+++ FF-1/common-utils/Constants.js
@@ -0,0 +1,3 @@
+'use strict';
+
+// TODO: abstract out any constants and export the module to consume in other files
