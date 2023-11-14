6c14707cae4991bbfcbc77c5dec5df458eec22be
Arjun Kalidas <arjunkalidas@live.in>
2023-11-13 23:26:39 -0500

    Update Constants.js
    - Adding constants for encryption/decryption mode
    - Adding constants for FPE modes

--- FF-1/common-utils/Constants.js
+++ FF-1/common-utils/Constants.js
@@ -1,3 +1,13 @@
 'use strict';
 
 // TODO: abstract out any constants and export the module to consume in other files
+
+const ENCRYPTION_MODE = "ENCYRPTION_MODE";
+
+const DECRYPTION_MODE = "DECRYPTION_MODE";
+
+const FPE_MODE = "FPE_MODE";
+
+const FF1 = "FF1";
+
+const FF3 = "FF3";
